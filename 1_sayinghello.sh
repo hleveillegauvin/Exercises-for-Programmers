5660bf659710212dce82372fda2f0917dcebd010
Hubert Léveillé Gauvin <31942487+hleveillegauvin@users.noreply.github.com>
2017-12-29 15:23:44 -0500

    Add files via upload

---
+++ 1_sayinghello.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Exercises for Programmers
+# 1 - Saying Hello
+# Written by: Hubert Léveillé Gauvin
+# Date: 15 December 2016, revised 13 September 2017
+
+# Create a program that prompts for your name and prints a greeting using your name.
+
+read -p "What is your name? " name
+echo "Hello, $name, nice to meet you!"
+
