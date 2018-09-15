262357be70134699cd1c86ccb41a2e9d2bb6f41f
Hubert Léveillé Gauvin <31942487+hleveillegauvin@users.noreply.github.com>
2018-09-15 16:46:20 -0400

    Add files via upload

---
+++ 01_sayinghello.ps1
@@ -0,0 +1,9 @@
+# Exercises for Programmers
+# 1 - Saying Hello
+# Written by: Hubert Léveillé Gauvin
+# Date: 15 September 2018
+
+# Create a program that prompts for your name and prints a greeting using your name.
+
+$name = Read-Host "What is your name?"
+Write-Host "Hello, $name, nice to meet you!"
