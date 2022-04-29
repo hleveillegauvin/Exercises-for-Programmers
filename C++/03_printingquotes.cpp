b4e58d8576ab9fe7007dd8a159904f74cff00b30
Hubert Léveillé Gauvin <31942487+hleveillegauvin@users.noreply.github.com>
2022-04-29 15:36:37 -0400

    Update 03_printingquotes.cpp

--- C++/03_printingquotes.cpp
+++ C++/03_printingquotes.cpp
@@ -1,9 +1,6 @@
-// 03_printingquotes.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 // Exercises for Programmers
 // 3 - Printing Quotes
-// Written by : Hubert Léveillé Gauvin
+// Written by : Hubert LÃ©veillÃ© Gauvin
 // Date : 29 April 2022
 
 // Create a program that prompts for a quoteand an author.Display the quotation using quotation marks.
@@ -49,17 +46,3 @@ std::string repeatPromptIfUserInputEmpty(std::string prompt)
    
     return answer;
 }
-
-
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
