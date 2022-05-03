3ff1640e22759f868eec5c403b3cafaa602a2142
Hubert Léveillé Gauvin <31942487+hleveillegauvin@users.noreply.github.com>
2022-05-02 20:06:00 -0400

    Update 04_madlib.cpp

--- C++/04_madlib.cpp
+++ C++/04_madlib.cpp
@@ -1,9 +1,6 @@
-// 04_madlib.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 // Exercises for Programmers
 // 4 - Mad Lib
-// Written by : Hubert Léveillé Gauvin
+// Written by : Hubert LÃ©veillÃ© Gauvin
 // Date : 02 May 2022
 
 // Create a simple mad - lib program that prompts for a noun, a verb, an adverb, and an adjectiveand injects those into a story.
@@ -52,18 +49,3 @@ std::string repeatPromptIfUserInputEmpty(std::string prompt)
     } while (answer.length() == 0);
     return answer;
 }
-
-
-
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
